2ad2bd8267a396fe87757a78bf50a5da6744aef4
Kamil <kamajovo@gmail.com>
2026-01-27 23:44:05 +0100

    chore: Initial project skeleton and architecture

---
+++ src/BitcoinTracking.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BitcoinTracking.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
